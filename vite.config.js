eccce1eaa43f060c5f0d57c0f910426a823db0f9
iammarvin7 <iammamrvin7@gmail.com>
2025-10-17 02:27:11 -0500

    Fix: Final build config and deploy assets

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: "/Amor-Chai_Website/", 
-<<<<<<< HEAD
 })
-=======
-})
->>>>>>> d990343ad18759493b400a23027db7022c17c69e
