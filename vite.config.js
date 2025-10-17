58603af9d8f1484e2661c80878c6e93771c0fad9
Marvyn007 <mrvnchdhr@gmail.com>
2025-10-17 02:23:32 -0500

    Fix formatting issues in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Amor-Chai_Website/", 
-<<<<<<< HEAD
+  base: "/Amor-Chai_Website/",
 })
-=======
-})
->>>>>>> d990343ad18759493b400a23027db7022c17c69e
